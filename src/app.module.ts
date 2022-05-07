20af413887931f0377eebfc6dd81423da47f3880
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2022-05-07 22:17:57 +0700

    [ADD]Upload Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserRoleModule} from './Masters/UserType/usersRole.module';
 import { ContentModule } from './Contents/content.module';
+import { MinioClientModule } from './Upload/minio-client/minio-client.module';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true,})
             ,MongooseModule.forRoot(process.env.DATABASE_URL)
-            ,UserRoleModule,ContentModule],
+            ,UserRoleModule,ContentModule,MinioClientModule],
 
   controllers: [AppController],
   providers: [AppService],
