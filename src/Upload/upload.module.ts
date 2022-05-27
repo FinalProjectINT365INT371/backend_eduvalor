5a56e5c037c51fdabdb27ba03dd6b1446aac641f
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2022-05-27 13:00:18 +0700

    [ADD]remove ssl

--- src/Upload/upload.module.ts
+++ src/Upload/upload.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [ ConfigModule.forRoot({ isGlobal: true,}),MinioModule.register({
     endPoint: process.env.ENDPOINT,
     port: parseInt(process.env.MINIO_PORT),
-    useSSL: true,
+    useSSL: false,
     accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY
   })],   
