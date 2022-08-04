3159dcb46b753d04ba4ffdefd40b6c4a87f8061c
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2022-08-04 17:19:56 +0700

    [ADD]Logger default service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { UserRoleModule} from './Masters/UserType/usersRole.module';
 import { ContentModule } from './Contents/content.module';
 import { UploadModule } from './Upload/upload.module';
 import { config } from 'aws-sdk';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -16,6 +19,36 @@ config.update({
   imports: [ConfigModule.forRoot({ isGlobal: true,})
             ,MongooseModule.forRoot(process.env.DATABASE_URL)
             ,UserRoleModule,ContentModule,UploadModule,
+            WinstonModule.forRoot({
+              format: winston.format.combine(
+                winston.format.timestamp({
+                  format: 'YYYY-MM-DD HH:mm:ss'
+                }),
+                winston.format.json(),
+              ),
+              transports: [
+                new winston.transports.Console({
+                  level: 'info',
+                  format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.printf(
+                      info => `${info.timestamp} ${info.level}: ${info.message}`
+                    )
+                  )
+                }),
+                new winston.transports.DailyRotateFile({
+                  level: 'debug',
+                  dirname:'./log/',
+                  filename: 'debug.log-%DATE%.log',
+                  datePattern: 'YYYY-MM-DD-HH',
+                }),
+                // new winston.transports.File({
+                //   dirname: './log/info/',
+                //   filename: 'info.log',
+                //   level: 'info',
+                // }),
+              ],
+            }),
         ],
 
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import { MinioService } from 'nestjs-minio-client';
-
+import { Injectable, Inject } from '@nestjs/common';
+import { Logger } from 'winston';
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(
+    @Inject('winston')
+    private readonly logger: Logger,){}
+    getHello(): string {
+    this.logger.info('Returning suggestions...');
+    this.logger.warn('Returning suggestions...');
     return 'Hello World!';
   }
 }
