dbb7148b1260a23ea77d7d1663da5555b6fb2def
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2022-08-05 17:14:11 +0700

    [FIX]Formate Code

--- src/Upload/upload.service.ts
+++ src/Upload/upload.service.ts
@@ -1,15 +1,12 @@
-import {
-  Inject,
-  Injectable,
-  UploadedFile,
-} from '@nestjs/common';
+import { Inject, Injectable, UploadedFile } from '@nestjs/common';
 import { Budgets, S3 } from 'aws-sdk';
 import { Logger } from 'winston';
 @Injectable()
 export class UploadService {
   constructor(
     @Inject('winston')
-    private readonly logger: Logger,) {}
+    private readonly logger: Logger,
+  ) {}
 
   // async listAllBuckets() {
   //   return this.minioClient.client.listBuckets();
@@ -84,28 +81,29 @@ export class UploadService {
   // S3 Service
   async uploadFile(@UploadedFile() file, Buckets: string, imageName: string) {
     const s3 = new S3();
-    const uploadResult = await s3.upload({
-      Bucket: Buckets,
-      Body: file.buffer,
-      Key: imageName,
-      ContentType: file.mimetype,
-    })
-      .promise(); 
+    const uploadResult = await s3
+      .upload({
+        Bucket: Buckets,
+        Body: file.buffer,
+        Key: imageName,
+        ContentType: file.mimetype,
+      })
+      .promise();
     this.logger.debug(`${'Uploaded : ' + imageName}`);
     return `${'Uploaded : ' + uploadResult.Key}`;
   }
 
   async removeImageS3(imageName: string, Buckets: string) {
     const s3 = new S3();
-    await s3.deleteObject({Bucket:Buckets , Key:imageName}).promise();
+    await s3.deleteObject({ Bucket: Buckets, Key: imageName }).promise();
     this.logger.debug(`Removed Images : ${imageName}`);
     return `Removed Images : ${imageName}`;
     //if not found FE use url and check HttpStatus
   }
 
-  async getSignedUrlS3(imageName: string, Buckets: string) {   
+  async getSignedUrlS3(imageName: string, Buckets: string) {
     const s3 = new S3();
-    const param = {'Bucket': Buckets, 'Key': imageName};
+    const param = { Bucket: Buckets, Key: imageName };
     this.logger.debug(param);
     const url = s3.getSignedUrl('getObject', param);
     this.logger.info(`${'imageUrl : ' + url}`);
@@ -114,26 +112,30 @@ export class UploadService {
 
   async getImageListS3(folder: string, Buckets: string) {
     const s3 = new S3();
-    var list = []
-    let param = { 'Bucket':  Buckets, 'Prefix': `${folder}`,};
-    await s3.listObjects(param, function(err, data) {
-      if (err) console.log(err, err.stack); 
-      else { 
-      this.logger.info(data); 
-      list = data.Contents;
-      }}).promise();  
-    return list ;
-  
+    var list = [];
+    let param = { Bucket: Buckets, Prefix: `${folder}` };
+    await s3
+      .listObjects(param, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else {
+          this.logger.info(data);
+          list = data.Contents;
+        }
+      })
+      .promise();
+    return list;
   }
 
   async getImageListS3InContent(items: [String]) {
     let imagesUrl = [];
     for (let item of items) {
-      let signedUrl = await this.getSignedUrlS3(item.toString(), 'eduvalor-contents');
+      let signedUrl = await this.getSignedUrlS3(
+        item.toString(),
+        'eduvalor-contents',
+      );
       signedUrl = signedUrl.toString().replace('imageUrl', item.toString());
       imagesUrl.push(signedUrl);
     }
     return imagesUrl;
   }
 }
-
