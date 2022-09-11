fd1c1c5626a69fa08b6571f6c92504e063ada077
Earty <worawit.4516@mail.kmutt.ac.th>
2022-09-11 15:50:18 +0700

    [FIX]config port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4000);
+  await app.listen(process.env.PORT_DEV);
   app.enableCors();
 }
 bootstrap();
