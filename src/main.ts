a409e2b13991508dea26eda7ed5c076369cafdc4
Dlz <pattarapol.plasma@mail.kmutt.ac.th>
2022-10-27 10:50:05 +0000

    Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT_PROD);
+  await app.listen(process.env.PORT_DEV);
   app.enableCors();
 }
 bootstrap();
